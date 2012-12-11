activeadmin/activeadmin

Fix linking to ActiveAdmin actions for models that define to_param

lib/active_admin/resource/action_items.rb
@@ -56,7 +56,7 @@ module ActiveAdmin
         # Edit link on show
         add_action_item :only => :show do
           if controller.action_methods.include?('edit')
-            link_to(I18n.t('active_admin.edit_model', :model => active_admin_config.resource_name), edit_resource_path(resource))
+            link_to(I18n.t('active_admin.edit_model', :model => active_admin_config.resource_name), edit_resource_path(resource.id))
           end
         end
 
@@ -64,7 +64,7 @@ module ActiveAdmin
         add_action_item :only => :show do
           if controller.action_methods.include?("destroy")
             link_to(I18n.t('active_admin.delete_model', :model => active_admin_config.resource_name),
-              resource_path(resource),
+              resource_path(resource.id),
               :method => :delete, :confirm => I18n.t('active_admin.delete_confirmation'))
           end
         end

getcomparisons/active_admin

Conflicts:
	lib/active_admin/resource/action_items.rb
	lib/active_admin/views/index_as_table.rb

lib/active_admin/resource/action_items.rb
@@ -61,7 +61,7 @@ module ActiveAdmin
         # Edit link on show
         add_action_item :only => :show do
           if controller.action_methods.include?('edit')
-            link_to(I18n.t('active_admin.edit_model', :model => active_admin_config.resource_label), edit_resource_path(resource))
+            link_to(I18n.t('active_admin.edit_model', :model => active_admin_config.resource_label), edit_resource_path(resource.id))
           end
         end
 
@@ -69,7 +69,7 @@ module ActiveAdmin
         add_action_item :only => :show do
           if controller.action_methods.include?("destroy")
             link_to(I18n.t('active_admin.delete_model', :model => active_admin_config.resource_label),
-              resource_path(resource),
+              resource_path(resource.id),
               :method => :delete, :data => {:confirm => I18n.t('active_admin.delete_confirmation')})
           end
         end